b1979a00adaa4391f5bea6aa3e81c9bd544d6413
artons98 <57039133+artons98@users.noreply.github.com>
2025-04-22 19:37:11 +0200

    pushsetup class -> static

--- push-setup.js
+++ push-setup.js
@@ -2,23 +2,24 @@ window.PageJS = window.PageJS || {};
 
 if(!PageJS.PushSetup){
     PageJS.PushSetup = class{
-        constructor(url = null, serviceWorkerPath = null){
-            this.vapidPublicKey = 'BJ_s73UJuOwb23NtDqmc5HoLhks1GAYL2h3_VGSFfoTiwZPsJTc_D4sqhIsMCbGloVG7BYGDkEakfwdaFpMebKs';
-            this.subscribeForPushNotifications(url, serviceWorkerPath);
-        }
-        async urlBase64ToUint8Array(base64String) {
+        // constructor(url = null, serviceWorkerPath = null){
+        //     this.vapidPublicKey = 'BJ_s73UJuOwb23NtDqmc5HoLhks1GAYL2h3_VGSFfoTiwZPsJTc_D4sqhIsMCbGloVG7BYGDkEakfwdaFpMebKs';
+        //     this.subscribeForPushNotifications(url, serviceWorkerPath);
+        // }
+        static vapidPublicKey = 'BJ_s73UJuOwb23NtDqmc5HoLhks1GAYL2h3_VGSFfoTiwZPsJTc_D4sqhIsMCbGloVG7BYGDkEakfwdaFpMebKs';
+        static async urlBase64ToUint8Array(base64String) {
             const padding = '='.repeat((4 - base64String.length % 4) % 4);
             const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
             return Uint8Array.from(atob(base64).split('').map(c => c.charCodeAt(0)));
         }
-        async subscribeForPushNotifications(url = null, serviceWorkerPath = null) {
+        static async subscribeForPushNotifications(url = null, serviceWorkerPath = null) {
             if (!('serviceWorker' in navigator)) return;
         
                 const registration = await navigator.serviceWorker.register( serviceWorkerPath ||'/service-worker.js');
                 const permission = await Notification.requestPermission();
             if (permission !== 'granted') return;
         
-            const key = await this.urlBase64ToUint8Array(vapidPublicKey);
+            const key = await this.urlBase64ToUint8Array(this.vapidPublicKey);
         
             const subscription = await registration.pushManager.subscribe({
                 userVisibleOnly: true,
@@ -30,6 +31,19 @@ if(!PageJS.PushSetup){
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(subscription)
             });
+        }
+        static async unSubscribeForPushNotifications(url = null) {
+            const registration = await navigator.serviceWorker.ready;
+            const sub = await registration.pushManager.getSubscription();
+            if (sub) {
+              await sub.unsubscribe();
+              console.log("Subscription opgezegd!");
+              await fetch(url || '/api/push/unsubscribe', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(sub)
+              });
+            }
         }
     } 
 }
