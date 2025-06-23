ce4bab16adabd726cb6ba24fd86dcc5a2c57129e
artons98 <57039133+artons98@users.noreply.github.com>
2025-06-23 20:50:19 +0200

    Namespace classes under PageJS

--- page-view.js
+++ page-view.js
@@ -1,4 +1,7 @@
-class PageView{
+window.PageJS = window.PageJS || {};
+
+if(!PageJS.PageView){
+    PageJS.PageView = class {
     constructor(title, viewModel, htmlPath, container = null, reInitialiseOnClose = true) {
         this.title = title;
         this.viewModel = viewModel;
@@ -54,6 +57,8 @@ class PageView{
         }
     
         return;
+    }
+
     }
 
 }
