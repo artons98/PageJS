b6a957699544156323e668b16c71cfa47b4ab568
artons98 <57039133+artons98@users.noreply.github.com>
2025-06-24 17:55:20 +0200

    added version.js

---
+++ version.js
@@ -0,0 +1,52 @@
+class VersionJS{
+    static init(versionFilePath){
+        window.VERSIONFILEPATH = versionFilePath;
+        this.checkVersionAndUpdateIfNeeded();
+    }
+    static async checkVersionAndUpdateIfNeeded() {
+        try {
+            const response = await fetch(`${window.VERSIONFILEPATH}?nocache=` + new Date().getTime());
+            const data = await response.json();
+            const currentVersion = data.version;
+            const savedVersion = localStorage.getItem('siteVersion');
+    
+            if (savedVersion !== currentVersion) {
+                console.log(`New version detected: ${currentVersion} (was ${savedVersion})`);
+                if ('caches' in window) {
+                    const keys = await caches.keys();
+                    await Promise.all(keys.map(k => caches.delete(k)));
+                }
+                localStorage.setItem('siteVersion', currentVersion);
+                try{
+                    if ("serviceWorker" in navigator) {
+                        var registration = await navigator.serviceWorker.getRegistration();
+                        registration.update();
+                    }
+                }catch{}
+                location.reload(true);
+            }
+        } catch (err) {
+            console.error('Version control failed:', err);
+        }
+    }
+    static waitForVariable = (name, callback, timeout = 10000) => {
+        const interval = 100;
+        let waited = 0;
+        const check = () => {
+        if (typeof window[name] === 'string') {
+            console.log(`[PageJS] variabele ${name} is beschikbaar en wordt gebruikt!`);
+            clearInterval(timer);
+            callback();
+        } else if (waited >= timeout) {
+            clearInterval(timer);
+            console.warn(`[PageJS] Timeout: variabele ${name} is niet gevonden`);
+        }
+            waited += interval;
+        };
+    
+        const timer = setInterval(check, interval);
+    };
+}
+VersionJS.waitForVariable("VERSIONFILEPATH", () => {
+    VersionJS.checkVersionAndUpdateIfNeeded();
+});
