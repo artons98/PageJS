739c30fb5612e96a68c47a92740eb5c81ebcff00
artons98 <57039133+artons98@users.noreply.github.com>
2025-06-25 23:31:20 +0200

    feat: add logo support

--- startup.js
+++ startup.js
@@ -7,6 +7,9 @@ if(!PageJS.Startup){
         if(PageJS.Config && typeof PageJS.Config.initialize === 'function'){
           await PageJS.Config.initialize();
         }
+        if(PageJS.Config && PageJS.Config.settings && PageJS.Config.settings.logo){
+          PageJS.Utils.applyLogoFromSettings();
+        }
         PageJS.Utils.waitForFunction("OnStartup", (fn) => {
           fn();
         });
